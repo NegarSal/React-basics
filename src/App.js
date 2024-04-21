de903a30cda4a2e55ed4da2f16d854387690fdb5
Negar <negarsaljooghi@gmail.com>
2024-04-21 10:36:57 +0330

    using regular js variable

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 
 function App() {
+  let name = 'mario'
+  const handleClick = () => {
+    name = 'luigi'
+    console.log(name)
+  }
   return (
     <div className="App">
-      
+      <h1>My name is {name}</h1>
+      <button onClick={handleClick}>Change name</button>
     </div>
   );
 }
