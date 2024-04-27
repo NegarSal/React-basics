29d11a2a63467c2ba3d7bd22611772154f49c5f6
Negar <negarsaljooghi@gmail.com>
2024-04-27 09:53:27 +0330

    Using Inline Styles & Dynamic Inline Styles

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,14 @@
 import ReactDom from 'react-dom'
 import './Modal.css'
 
-export default function Modal({ children, handleClose }) {
+export default function Modal({ children, handleClose, isSalesModal }) {
     return ReactDom.createPortal((
         <div className="modal-backdrop">
-            <div className="modal">
+            <div className="modal" style={{
+                border: "4px solid",
+                borderColor: isSalesModal ? "#ff4500" : "#555",
+                textAlign: "center"
+            }}>
                 {children}
                 <button onClick={handleClose}>close</button>
             </div>
