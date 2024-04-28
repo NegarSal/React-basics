9e7cf96e2711ea6f2633b6614a1f0901efbbd2fa
Negar <negarsaljooghi@gmail.com>
2024-04-28 09:28:40 +0330

    Controlled Inputs

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -5,18 +5,32 @@ export default function NewEventForm() {
     const [title, setTitle] = useState('')
     const [date, setDate] = useState('')
 
+    const resetForm = () => {
+        setTitle('')
+        setDate('')
+    }
+
     return (
         <form className='new-event-form'>
             <label>
                 <span>EventTitle:</span>
-                <input type='text' onChange={e => setTitle(e.target.value)} />
+                <input
+                    type='text'
+                    onChange={e => setTitle(e.target.value)}
+                    value={title}
+                />
             </label>
             <label>
                 <span>EventDate:</span>
-                <input type='date' onChange={e => setDate(e.target.value)} />
+                <input
+                    type='date'
+                    onChange={e => setDate(e.target.value)}
+                    value={date}
+                />
             </label>
             <button>Submit</button>
             <p>title- {title}, date- {date}</p>
+            <p onClick={resetForm}>rest the form</p>
         </form>
     )
 }
