f16c73b2557c810363685d6cfa06be4e805f090d
Negar <negarsaljooghi@gmail.com>
2024-04-28 11:10:45 +0330

    Using the useRef Hook

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -1,25 +1,37 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import './NewEventForm.css'
 
 export default function NewEventForm({ addEvent }) {
-    const [title, setTitle] = useState('')
-    const [date, setDate] = useState('')
+    // const [title, setTitle] = useState('')
+    // const [date, setDate] = useState('')
+    const title = useRef()
+    const date = useRef()
 
     const resetForm = () => {
-        setTitle('')
-        setDate('')
+        //setTitle('')
+        //setDate('')
+        title.current.value = ""
+        date.current.value = ""
     }
 
     const handelSubmit = e => {
         e.preventDefault()
-
+        console.log(title, date)
         const event = {
-            title: title,
-            date: date,
+            title: title.current.value,
+            date: date.current.value,
             id: Math.floor(Math.random() * 10000)
         }
         addEvent(event)
         resetForm()
+
+        // const event = {
+        //     title: title,
+        //     date: date,
+        //     id: Math.floor(Math.random() * 10000)
+        // }
+        // addEvent(event)
+        // resetForm()
     }
 
     return (
@@ -28,16 +40,18 @@ export default function NewEventForm({ addEvent }) {
                 <span>EventTitle:</span>
                 <input
                     type='text'
-                    onChange={e => setTitle(e.target.value)}
-                    value={title}
+                    //onChange={e => setTitle(e.target.value)}
+                    //value={title}
+                    ref={title}
                 />
             </label>
             <label>
                 <span>EventDate:</span>
                 <input
                     type='date'
-                    onChange={e => setDate(e.target.value)}
-                    value={date}
+                    //onChange={e => setDate(e.target.value)}
+                    //value={date}
+                    ref={date}
                 />
             </label>
             <button>Submit</button>
